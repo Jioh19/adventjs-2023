2715432a4264c0691c5a6524801b83b0c93f05bf
Jioh19 <jioh1982@gmail.com>
2023-12-04 11:59:49 -0300

    fix 04

--- 04/script.ts
+++ 04/script.ts
@@ -1,16 +1,12 @@
 function decode(message: string) {
-  while(message.includes("(")){
-    const s1 = message.lastIndexOf('(');
-    const e1 = message.indexOf(')', s1);
-    if(s1 == -1) return message;
-    const slice1 = message.slice(s1+1, e1);
-    let cambio = '';
-    for (let i = slice1.length - 1; i >= 0; i--) {
-      cambio += slice1[i];
-    }
-    message = message.slice(0, s1) + cambio + message.slice(e1+1);
-  }
-  return message;
+	while (message.includes('(')) {
+		const s1 = message.lastIndexOf('(');
+		const e1 = message.indexOf(')', s1);
+		const slice1 = message.slice(s1 + 1, e1);
+		const cambio = slice1.split('').reverse().join('');
+		message = message.slice(0, s1) + cambio + message.slice(e1 + 1);
+	}
+	return message;
 }
 
 const a = decode('hola (odnum)');
