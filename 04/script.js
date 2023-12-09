89fc008c24682beab81cfd270a38d93725ea936c
Jioh19 <jioh1982@gmail.com>
2023-12-09 11:29:09 -0300

    trim files

--- 04/script.js
+++
@@ -1,9 +0,0 @@
-function decode(message: string) {
-  do {
-    const s1 = message.lastIndexOf('(');
-    const e1 = message.indexOf(')', s1);
-    const cambio = message.slice(s1+1, e1).split("").reverse().join("");
-    message = message.slice(0, s1) + cambio + message.slice(e1+1);
-  } while(message.includes("("))
-  return message;
-}
