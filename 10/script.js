bb5171a65bfb20ac923cd3e69bd6eb65da72a9cf
Jioh19 <jioh1982@gmail.com>
2023-12-10 12:50:09 -0300

    done 10 v1.1

--- 10/script.js
+++ 10/script.js
@@ -1,23 +1,20 @@
 function createChristmasTree(ornaments, height) {
-    console.log(ornaments, height);
-    var index = 0;
-    var result = '';
-    for (var i = 0; i < height; i++) {
-        var line = ' '.repeat(height - 1 - i);
-        for (var j = 0; j < i + 1; j++) {
-            line += "".concat(ornaments[index], " ");
-            if (index == ornaments.length - 1) {
-                index = 0;
-            }
-            else {
-                index++;
-            }
-        }
-        line = line.trimEnd();
-        line += '\n';
-        result += line;
-    }
-    result += ' '.repeat(height - 1) + '|\n';
-    return result;
+
+	let result = ' '.repeat(height - 1);
+	let counter = 1;
+
+	for (let i = 0; i < (height * (height + 1)) / 2; i++) {
+		if ((counter * (counter + 1)) / 2 == i) {
+			counter++;
+            result = result.slice(0, -1)
+			result += '\n' + ' '.repeat(height - counter);
+		}
+		result += `${ornaments[i % ornaments.length]} `;
+	}
+    result = result.slice(0, -1);
+	result += '\n' + ' '.repeat(height - 1) + '|\n';
+
+	return result;
 }
-console.log(createChristmasTree('123', 4));
+
+console.log(createChristmasTree('123', 5));
--- 10/script.ts
+++ 10/script.ts
@@ -1,23 +1,19 @@
 function createChristmasTree(ornaments: string, height: number): string {
-	console.log(ornaments, height);
-	let index = 0;
-	let result = '';
-	for (let i = 0; i < height; i++) {
-		let line = ' '.repeat(height - 1 - i);
-		for (let j = 0; j < i + 1; j++) {
-			line += `${ornaments[index]} `;
-			if (index == ornaments.length - 1) {
-				index = 0;
-			} else {
-				index++;
-			}
+	let result = ' '.repeat(height - 1);
+	let counter = 1;
+
+	for (let i = 0; i < (height * (height + 1)) / 2; i++) {
+		if ((counter * (counter + 1)) / 2 == i) {
+			counter++;
+			result = result.slice(0, -1);
+			result += '\n' + ' '.repeat(height - counter);
 		}
-		line = line.trimEnd();
-		line += '\n';
-		result += line;
+		result += `${ornaments[i % ornaments.length]} `;
 	}
-	result += ' '.repeat(height - 1) + '|\n';
+	result = result.slice(0, -1);
+	result += '\n' + ' '.repeat(height - 1) + '|\n';
+
 	return result;
 }
 
-console.log(createChristmasTree('123', 4));
+console.log(createChristmasTree('123', 5));
