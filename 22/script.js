203d5bb3e890f64ec0710ef49c630f98da5a778d
Jioh19 <jioh1982@gmail.com>
2023-12-23 13:24:50 -0300

    done 23

--- 22/script.js
+++ 22/script.js
@@ -1,18 +1,27 @@
 function compile(code) {
-    var counter = 0;
-    var ret = null;
-    for (var i = 0; i < code.length; i++) {
-        counter += +(code[i] == '+');
-        counter -= +(code[i] == '-');
-        counter = [counter, counter * 2][+(code[i] == '*')];
-        ret = [ret, i][+(code[i] == '%')];
-        if (ret != null && code[i] == '<') {
-            i = ret;
-            ret = null;
-        }
-        i = [i, code.indexOf('?')][+(counter <= 0 && code[i] == '¿')];
-    }
-    return counter;
+	let counter = 0;
+	let ret = null;
+	for (let i = 0; i < code.length; i++) {
+		counter += +(code[i] == '+');
+		counter -= +(code[i] == '-');
+
+		counter = [counter, counter * 2][+(code[i] == '*')];
+
+		ret = [ret, counter][+(code[i] == '%')];
+		// counter += [0, counter - ret][+(ret != null) * +(code[i] == '<')];
+		// ret = [ret, null][+(ret != null) * +(code[i] == '<')];
+
+		console.log(counter, ret)
+		// const condicion = [
+		// 	(counter <= 0),
+		// 	(code[i] == '<'),
+		// ][+(counter <= 0 < code[i] == '<')];
+		[counter, ret] = [[0, ret],[counter - ret, null]][+(counter <= 0) * +(code[i] == '<')];
+		console.log(counter, ret)
+
+		i = [i, code.indexOf('?')][+(counter <= 0) * +(code[i] == '¿')];
+	}
+	return counter;
 }
 console.log(compile('++*-'));
 console.log(compile('++%++<'));
